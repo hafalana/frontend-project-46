294f4e5ccd0874448ebbd31549504c67df100999
hafalana <hafal.ana@yandex.ru>
2025-04-09 18:03:56 +0400

    step 4
    add difference

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/hafalana/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/hafalana/frontend-project-46/actions)
+
+difference:
+https://asciinema.org/a/fZCCzOAMMHQaTYiyJxcEgRTMr
--- bin/gendiff.mjs
+++ bin/gendiff.mjs
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import readFile from "../src/fileParse.js";
+import { Command } from 'commander';
+import readFile from '../src/fileParse.js';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
@@ -8,23 +10,19 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>', 'path to the first configuration file')
   .argument('<filepath2>', 'path to the second configuration file')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .version('0.0.1')
 
 program.action((filepath1, filepath2) => {
+  try {
   const fileData1 = readFile(filepath1);
   const fileData2 = readFile(filepath2);
 
-  console.log('FileData 1:', fileData1);
-  console.log('FileData 2:', fileData2);
+  const difference = genDiff(fileData1, fileData2);
+  console.log(difference);
+  } catch (error) {
+    console.error(`Error reading files: ${error.message}`);
+  }
 
 });
 program.parse(process.argv);
-
-const genDiff = (filepath1, filepath2) => {
-    const diff = {};
-    return diff;
-};
-
-
-export default genDiff;
